1d2ad71dc55ac96ba0a60062268dd1b8023bb216
Soung Oo Lwin <soungoolwin275@gmail.com>
2023-08-01 21:01:00 +0700

    Footer Frame Done

---
+++ src/components/Footer.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="footer">
+    <div class="container w-[80%] mx-auto px-4 mt-10 py-10">
+      <div class="footerResponsive">
+        <div>
+          <p class="text-[#69727A]">&copy; Gold House International CO.,LTD</p>
+        </div>
+        <div>
+          <p class="footerHeading">Address:</p>
+          <p class="footerText">
+            No(3), Thumingalar Road, Thumingalar Housing, Thingangyun T/S,
+            Yangon, Myanmar
+          </p>
+
+          <p class="footerHeading">Phone:</p>
+          <p class="footerText">
+            <a href="tel:+95 9759528528">+95 9759528528</a>
+          </p>
+
+          <p class="footerHeading">Email:</p>
+          <p class="footerText">
+            <a href="mailto:kyawkhinethein@gmail.com"
+              >kyawkhinethein@gmail.com</a
+            >
+          </p>
+        </div>
+        <div>
+          <p class="footerHeading">Quick Links</p>
+          <p class="footerLink">Home</p>
+          <p class="footerLink">Projects</p>
+          <p class="footerLink">Services</p>
+          <p class="footerLink">Contact</p>
+        </div>
+        <div>
+          <p class="footerHeading">Social</p>
+          <p class="footerSocial">
+            <i class="fa-brands fa-facebook"></i>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.footer {
+  @apply bg-[#343A3F] h-auto;
+}
+.footerResponsive {
+  @apply grid gap-5 grid-cols-1 sm:grid-cols-2 md:grid-cols-4;
+}
+.footerText {
+  @apply text-[#ffff] hover:text-blue-500 cursor-pointer;
+}
+.footerHeading {
+  @apply text-[#7D868E] mb-2;
+}
+.footerLink {
+  @apply text-[#ffff] hover:text-blue-500 mb-2;
+}
+.footerSocial {
+  @apply text-[#ffff];
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,16 @@
 <template>
   <HeroSection />
   <Services />
+  <Footer></Footer>
 </template>
 
 <script>
+import Footer from "../components/Footer";
 import Services from "../components/Services";
 import HeroSection from "../components/HeroSection";
 export default {
   components: {
+    Footer,
     Services,
     HeroSection,
   },
