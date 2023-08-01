47edf2c6b2eef9ba170258dd81e8f293debc7b91
Soung Oo Lwin <soungoolwin275@gmail.com>
2023-08-01 20:19:29 +0700

    Services Section Done

--- src/components/Services.vue
+++ src/components/Services.vue
@@ -1,10 +1,11 @@
 <template>
-  <h2 class="text-center my-10 font-bold">Our Services</h2>
+  <h2 class="servicesHeading">Our Services</h2>
   <div class="flex justify-center">
-    <div class="w-[150px] h-1 bg-yellow-400"></div>
+    <div class="yellowLine"></div>
   </div>
+  <!-- First Row -->
   <div class="container mx-auto px-4 my-10">
-    <div class="grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
+    <div class="firstrowResponsive">
       <div class="p-4 flex">
         <div class="w-30">
           <img
@@ -18,31 +19,35 @@
         <div class="w-70 px-4">
           <h1 class="text-xl mb-2 font-bold">Highway Enginerring</h1>
           <p class="text-sm">
-            We do big highway projects both international and domestic.
+            We do highway projects with expertise in engineering design and
+            testing, delivering safe, innovative, and precise solutions
+            professionally. We do Highway Maintenance Engineering as well.
           </p>
         </div>
       </div>
       <div class="p-4 flex">
         <div class="w-30">
           <img
-            src="../../public/img/highway.png"
+            src="../../public/img/municipal.png"
             alt=""
-            width="100px"
+            width="60px"
             height="100px"
           />
         </div>
 
         <div class="w-70 px-4">
-          <h1 class="text-xl mb-2 font-bold">Highway Enginerring</h1>
+          <h1 class="text-xl mb-2 font-bold">Municipal Enginerring</h1>
           <p class="text-sm">
-            We do big highway projects both international and domestic.
+            We offer comprehensive Municipal Engineering services, encompassing
+            planning, design, and implementation, to build sustainable and
+            thriving communities efficiently.
           </p>
         </div>
       </div>
       <div class="p-4 flex">
         <div class="w-30">
           <img
-            src="../../public/img/highway.png"
+            src="../../public/img/housing.png"
             alt=""
             width="100px"
             height="100px"
@@ -50,51 +55,42 @@
         </div>
 
         <div class="w-70 px-4">
-          <h1 class="text-xl mb-2 font-bold">Highway Enginerring</h1>
+          <h1 class="text-xl mb-2 font-bold">Housing Construction</h1>
           <p class="text-sm">
-            We do big highway projects both international and domestic.
+            We specialize in Housing Construction, creating exceptional homes
+            with quality craftsmanship and attention to detail, tailored to your
+            dreams.
           </p>
         </div>
       </div>
-
+    </div>
+  </div>
+  <!-- Second Row -->
+  <div class="container sm:w-[60%] px-4 mx-auto mb-[65px]">
+    <div class="secondrowResponsive">
       <div class="p-4 flex">
         <div class="w-30">
           <img
-            src="../../public/img/highway.png"
+            src="../../public/img/bridge.png"
             alt=""
             width="100px"
-            height="100px"
+            height="200px"
           />
         </div>
 
         <div class="w-70 px-4">
-          <h1 class="text-xl mb-2 font-bold">Highway Enginerring</h1>
+          <h1 class="text-xl mb-2 font-bold">Bridge Enginerring</h1>
           <p class="text-sm">
-            We do big highway projects both international and domestic.
-          </p>
-        </div>
-      </div>
-      <div class="p-4 flex">
-        <div class="w-30">
-          <img
-            src="../../public/img/highway.png"
-            alt=""
-            width="100px"
-            height="100px"
-          />
-        </div>
-
-        <div class="w-70 px-4">
-          <h1 class="text-xl mb-2 font-bold">Highway Enginerring</h1>
-          <p class="text-sm">
-            We do big highway projects both international and domestic.
+            We excel in designing, constructing, and inspecting bridges,
+            ensuring durability, safety, and seamless integration into
+            transportation networks.
           </p>
         </div>
       </div>
       <div class="p-4 flex">
         <div class="w-30">
           <img
-            src="../../public/img/highway.png"
+            src="../../public/img/metrology.png"
             alt=""
             width="100px"
             height="100px"
@@ -102,9 +98,10 @@
         </div>
 
         <div class="w-70 px-4">
-          <h1 class="text-xl mb-2 font-bold">Highway Enginerring</h1>
+          <h1 class="text-xl mb-2 font-bold">Enginerring Metrology</h1>
           <p class="text-sm">
-            We do big highway projects both international and domestic.
+            We provide Quality Management Certificates, ensuring top-notch
+            precision and excellence in all measurements.
           </p>
         </div>
       </div>
@@ -116,4 +113,19 @@
 export default {};
 </script>
 
-<style scoped></style>
+<style scoped>
+.servicesHeading {
+  @apply text-center my-10 font-bold;
+}
+
+.yellowLine {
+  @apply w-[150px] h-1 bg-yellow-400;
+}
+
+.firstrowResponsive {
+  @apply grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3;
+}
+.secondrowResponsive {
+  @apply grid gap-4 grid-cols-1 sm:grid-cols-2;
+}
+</style>
